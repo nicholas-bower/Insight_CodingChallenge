be7be3715aa6d74c346538bd698d56e19c7611b4
Nicholas_bower@alumni.brown.edu <nbower@DESKTOP-QQFJTT7.localdomain>
2018-07-17 01:58:41 -0400

    Changed location of sample input

--- insight_testsuite/temp/run.sh
+++ insight_testsuite/temp/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #This simply runs the program
 #I've included the default arguments so they can more easily be changed if need be
-python ./src/Pharma_DataAnalyzer.py -i ./input/reduced_de_cc_data.txt -h 1 -n -1 -o ./output/top_cost_drug.txt
+python ./src/Pharma_DataAnalyzer.py -i ./input/itcont.txt -h 1 -n -1 -o ./output/top_cost_drug.txt
