4d87f2188774a83ccff5aa85ccd06760eadc9f18
nicholas-bower <nicholas_bower@alumni.brown.edu>
2018-07-16 22:51:33 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#
-# Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
-#
-python ./src/pharmacy_counting.py 
+#This simply runs the program
+#I've included the default arguments so they can more easily be changed if need be
+python ./src/pharmacy_counting.py python /src/Pharma_DataAnalyzer.py -i ./input/reduced_de_cc_data.txt -h 1 -n -1 -o ./output/top_cost_drug.txt
